5025dcb5232176fea9540f16ef8c27a517fa4320
Jorge Rodriguez <jorge.rodriguez@webmaps.com.mx>
2018-04-23 17:25:59 -0500

    Se crea un webview para hacer uso de SwipeRefresh Layout

--- app/src/main/java/mx/com/webmaps/md_ejercicio15/MainActivity.java
+++ app/src/main/java/mx/com/webmaps/md_ejercicio15/MainActivity.java
@@ -1,13 +1,53 @@
 package mx.com.webmaps.md_ejercicio15;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout swipeRefreshLayout;
+    WebView webView;
+    WebViewClient webViewClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById( R.id.swipeRefresh_id);
+        webView =(WebView)findViewById(R.id.webView_id);
+
+        webView.loadUrl("http://nba.com");
+
+        webViewClient= new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                System.out.println("finish");
+            }
+
+            @Override
+            public void onPageCommitVisible(WebView view, String url) {
+                super.onPageCommitVisible(view, url);
+
+                System.out.println("On page commit visible");
+            }
+
+            @Override
+            public void onLoadResource(WebView view, String url) {
+                super.onLoadResource(view, url);
+
+                System.out.println("on load");
+            }
+        };
+
+
+
+        webView.setWebViewClient(webViewClient);
+
     }
 }
